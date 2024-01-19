5b1c0c9e31dfdce9199264890362a798e4d76f10
Pavithra8805 <pavithra8805@gmail.com>
2024-01-19 15:15:07 +0000

    First commit

---
+++ hof/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HigherOrderFunctions from './Components/HigherOrderFunctions';
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <HigherOrderFunctions />
+    </>
+  );
+}
+
+export default App;
